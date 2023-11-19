6129b7e1e1672b542b0a9659bad93ab437aceee3
Ishikajain02 <ishijain02@gmail.com>
2023-11-20 02:01:33 +0530

    sidebar_working

--- src/components/Head.js
+++ src/components/Head.js
@@ -7,10 +7,13 @@ const Head = () => {
    const toggleMenuHandler = ()=>{
     dispatch(toggleMenu());
    }
+ 
    return(
   <div className=" grid grid-cols-12 gap-4 shadow-md p-4">
     <div className="cols col-span-3 flex flex-row ">
+      
       <img onClick={()=>toggleMenuHandler()}className="h-8 w-8 cursor-pointer " alt="youtube" src="https://cdn.iconscout.com/icon/free/png-256/free-hamburger-menu-462145.png?f=webp"></img>
+      
       <img className="h-8 w-8 ml-4" alt="icon" src="https://static-00.iconduck.com/assets.00/youtube-icon-2048x1450-n77mmhe0.png"></img>
       
 
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,9 +1,11 @@
 import React from 'react'
-
+import { useSelector } from 'react-redux';
 const Sidebar = () => {
+  const isMenuOpen = useSelector((store)=> store.app.isMenuOpen);
+  if(isMenuOpen!=true)return null;
   return (
     <>
-    <div className="flex flex-col">
+    <div className="flex flex-col shadow-lg w-60">
         <div className="p-7 flex flex-col">
          Home
         </div>
