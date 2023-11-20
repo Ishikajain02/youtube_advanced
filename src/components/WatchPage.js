db89ecfceba48125c79574c34a9d6ba2588b1fa1
Ishikajain02 <ishijain02@gmail.com>
2023-11-21 03:08:27 +0530

    feat:Watch_Page_working

--- src/components/WatchPage.js
+++ src/components/WatchPage.js
@@ -1,16 +1,26 @@
 import { useDispatch } from "react-redux";
 import { useEffect } from "react";
 import { closeMenu } from "../utils/appSlices";
-const WatchPage=()=>{
-    const dispatch = useDispatch();
-    useEffect(()=>{
-      dispatch(closeMenu());
-    },[])
-    return(
-        <>
-        <h1>watch</h1>
-
-        </>
-    )
-}
+import { useSearchParams } from "react-router-dom";
+const WatchPage = () => {
+  const dispatch = useDispatch();
+  const [serachParams] = useSearchParams();
+ console.log( serachParams.get("v"));
+  useEffect(() => {
+    dispatch(closeMenu());
+  }, []);
+  return (
+    <>
+      <iframe
+        width="1000"
+        height="500"
+        src={"https://www.youtube.com/embed/"+ serachParams.get("v")} 
+        title="YouTube video player"
+        frameBorder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        allowFullScreen
+      ></iframe>
+    </>
+  );
+};
 export default WatchPage;
