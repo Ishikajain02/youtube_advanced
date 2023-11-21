1d4d73d94289f9be7a793929010ff0de5f6b07b1
Ishikajain02 <ishijain02@gmail.com>
2023-11-22 02:57:11 +0530

    feat:debouncing_search_updated

--- src/components/Head.js
+++ src/components/Head.js
@@ -1,13 +1,28 @@
-import React from 'react';
+import React,{useState,useEffect} from 'react';
 import { useDispatch } from 'react-redux';
 import { toggleMenu } from '../utils/appSlices';
 
 const Head = () => {
    const dispatch = useDispatch();
+   const[searchquery ,setsearchquery]=useState("");
    const toggleMenuHandler = ()=>{
     dispatch(toggleMenu());
    }
- 
+   const  search = async ()=>{
+    const data = await fetch("https://www.googleapis.com/youtube/v3/search?key=AIzaSyChlI_oQ9vEBT_VB7xzLlyNSfxK5rv8Kco%20&q="+ searchquery);
+   //hide api keys and api
+    const json = await data.json();
+    console.log("api");
+    console.log(json);
+}
+   useEffect(()=>{
+    const timer =setTimeout(()=>search(),200);
+      // search();
+ return ()=>{
+  clearTimeout(timer);
+ }
+   },[searchquery]);
+   
    return(
   <div className=" grid grid-cols-12 gap-4 shadow-md p-4">
     <div className="cols col-span-3 flex flex-row ">
@@ -20,7 +35,7 @@ const Head = () => {
     </div>
     
       <div className=' cols col-span-6 flex flex-row'>
-      <input type="search" placeholder="Search " className="border-b-2 w-11/12 rounded-l-3xl  border-gray-200 py-3 px-7"></input>
+      <input type="search" value={searchquery} onChange={(e)=>{setsearchquery(e.target.value)}}placeholder="Search " className="border-b-2 w-11/12 rounded-l-3xl  border-gray-200 py-3 px-7"></input>
       <button className="h-12 bg-gray-200 py-3 px-7 w-14 rounded-r-3xl">
       🔍
       </button>
